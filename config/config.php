f34d44a039c1e47bc83370704cfc63b021b31f0c
Mike Smullin <mike@smullindesign.com>
2011-08-19 00:51:42 -0600

    raising token expire times back to normal

--- config/config.php
+++ config/config.php
@@ -1,18 +1,14 @@
 <?php
 
 $config['OAuth2Server'] = array(
-	'access_token_lifetime'		=> 60*2, // minutes
-	'auth_code_lifetime' 		=> 60*2, // minutes
-	'refresh_token_lifetime'	=> 60*4, // minutes
-
-// 	'access_token_lifetime'		=> 60*60*12, // 12 hours
-// 	'auth_code_lifetime' 		=> 60*60*12, // 12 hours
-// 	'refresh_token_lifetime'	=> 60*60*24*7*2, // 2 weeks
+	'access_token_lifetime'		=> 60*60*12, // 12 hours
+	'auth_code_lifetime' 		=> 60*60*12, // 12 hours
+	'refresh_token_lifetime'	=> 60*60*24*7*2, // 2 weeks
 
 	'Auth' => array(
 		'className' => 'ClipmunkAuth',
 		'fields' => array(
-			'username' => 'username',
+			'username' => 'email',
 			'password' => 'password'
 		)
 	)
