e703f581b529f384e28da6e29da70d14f4e67241
Mike Smullin <mike@smullindesign.com>
2011-08-19 00:41:11 -0600

    configure custom ClipmunkAuthComponent which supports username, email, or phone. temporarily set access_token expire to 2 min and refresh_token to 4 min.

--- config/config.php
+++ config/config.php
@@ -1,14 +1,18 @@
 <?php
 
 $config['OAuth2Server'] = array(
-	'access_token_lifetime'		=> 60*60*12, // 12 hours
-	'auth_code_lifetime' 		=> 60*60*12, // 12 hours
-	'refresh_token_lifetime'	=> 60*60*24*7*2, // 2 weeks
+	'access_token_lifetime'		=> 60*2, // minutes
+	'auth_code_lifetime' 		=> 60*2, // minutes
+	'refresh_token_lifetime'	=> 60*4, // minutes
+
+// 	'access_token_lifetime'		=> 60*60*12, // 12 hours
+// 	'auth_code_lifetime' 		=> 60*60*12, // 12 hours
+// 	'refresh_token_lifetime'	=> 60*60*24*7*2, // 2 weeks
 
 	'Auth' => array(
-		'className' => 'Auth',
+		'className' => 'ClipmunkAuth',
 		'fields' => array(
-			'username' => 'email',
+			'username' => 'username',
 			'password' => 'password'
 		)
 	)
